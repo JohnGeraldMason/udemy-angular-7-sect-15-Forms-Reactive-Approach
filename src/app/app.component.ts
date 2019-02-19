95c11d816506a39c75cf2cd2ef0e734c1c92f7d4
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-02-19 15:22:26 -0400

    Created async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,5 +39,17 @@ export class AppComponent implements OnInit {
     }
     return null;
   }
-
+  
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
